ed857eda9f5e1b233ba493afed8f080efeb7a041
zenah12 <zena.hadjivasiliou@ucl.ac.uk>
2021-10-12 13:55:56 +0100

    Delete InitiationFunctions.hpp

--- InitiationFunctions.hpp
+++
@@ -1,9 +0,0 @@
-void Init_N(double mat[numx]);
-void Init_NT(double mat[numx][numt/store_n]);
-void Init_FRAP_logs(double mat[2][numt/store_n+1]);
-void Init_3(double mat[3], double par, double min, double max);
-void Init_Params(double mat[14][T_th]);
-void Init_FRAPrec(double mat[datN]);
-void Initiate_Profile_SS(double mat[numx], int index);
-void Initiate_source(double source[numx]);
-
